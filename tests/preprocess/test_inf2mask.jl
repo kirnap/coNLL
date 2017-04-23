722278a30d3c84f26435fc4832dcd629fa1c391d
okirnap <okirnap@somon.hpc.ku.edu.tr>
2017-04-23 19:28:16 +0300

    add new char lookup test

--- tests/preprocess/test_inf2mask.jl
+++ tests/preprocess/test_inf2mask.jl
@@ -17,7 +17,7 @@ function test_inf2mask()
     ids = nextbatch(ptb, s, out_vocab, all_vocab, batchsize; maxlines=100)
 
     # get random batch of words
-    ch1 = create_chvocab("../../ptb/ptb_all_vocab")
+    ch1 = create_chvocab(all_vocab) #("../../ptb/ptb_all_vocab")
     i2c = Array(Char, length(ch1))
     for (k, v) in ch1; i2c[v] = k; end;
     wids = ids[rand(1:length(ids))]
@@ -42,9 +42,24 @@ function test_inf2mask()
         push!(pchs, ds)
     end
     for i=1:length(pchs[1]); for item in pchs; print("$(item[i]) ");end;println();end;
-
     
-    return (data, masks, i2c)
+    (data2, masks2) = charlup3(wids, i2w_all, ch1)
+    pchs2 = Any[]
+    for i=1:length(data2)
+        item = data2[i]; mitem = masks2[i];
+        chars = Any[]
+        for c in item;append!(chars, i2c[c]);end;
+        ds = Any[]
+        for k=1:length(chars)
+            x = (chars[k], mitem[k]) # pm for put mask
+            push!(ds, x)
+        end
+        push!(pchs2, ds)
+    end
+    println("\n3rd version of charlup-->")
+    for i=1:length(pchs2[1]); for item in pchs2; print("$(item[i]) ");end;println();end;
+
+    return (data2, i2w_all, ch1)
 
 end
 !isinteractive() && test_inf2mask()
